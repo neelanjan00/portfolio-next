95df092d159ef8c34c54ee6c5a04841a1c17c276
Neelanjan Manna <neelanjan.manna@harness.io>
2022-06-19 12:08:59 +0530

    updated default head public url
    Signed-off-by: Neelanjan Manna <neelanjan.manna@harness.io>

--- src/heads/default-head.js
+++ src/heads/default-head.js
@@ -32,7 +32,7 @@ export default function DefaultHead() {
             <meta property="og:title" content="Neelanjan Manna" key="og-title" />
             <meta property="og:type" content="website" key="og-type" />
             <meta property="og:url" content="https://neelanjan.dev/" key="og-url" />
-            <meta property="og:image" content="/portrait.png" key="og-image" />
+            <meta property="og:image" content="%PUBLIC_URL%/portrait.png" key="og-image" />
             <meta property="og:image:width" content="500" key="og-image-width" />
             <meta property="og:image:height" content="500" key="og-image-height" />
             <meta property="og:image:alt" content={title} key="og-image-alt" />
@@ -41,10 +41,10 @@ export default function DefaultHead() {
             <meta name="twitter:creator" content="Neelanjan Manna" />
             <meta name="twitter:title" content="Neelanjan Manna" key="twitter-title" />
             <meta name="twitter:description" content="Neelanjan is an SDE at Harness. He is a cloud-native enthusiast who contributes to the development of the LitmusChaos project." key="twitter-description" />
-            <meta name="twitter:image" content="/portrait.png" key="twitter-image" />
+            <meta name="twitter:image" content="%PUBLIC_URL%/portrait.png" key="twitter-image" />
             <meta name="twitter:site" content="@NeelanjanManna" />
             <meta name="twitter:creator" content="@NeelanjanManna" />
-            <link rel='icon' href='/favicon.ico' />
+            <link rel='icon' href='%PUBLIC_URL%/favicon.ico' />
             <meta name="robots" content="index, follow" />
             <meta name="page-topic" content="Media" />
             <meta name="page-type" content="Blogging" />
